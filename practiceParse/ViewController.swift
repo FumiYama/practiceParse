0d07cb6c22eff1db3d2a30a19419b5ae7e3bc00c
FumiYama <f.yamanaka1127@gmail.com>
2015-09-07 23:12:36 +0900

    Initial Commit

---
+++ practiceParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practiceParse
+//
+//  Created by Fumiya Yamanaka on 2015/09/07.
+//  Copyright (c) 2015年 Fumiya Yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
